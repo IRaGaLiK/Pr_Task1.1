0e36b5edcad8f357b29f039f94203a75c6dad5fd
RaGaLiK <RaGaLiK@DESKTOP-58NBP1E>
2023-05-06 17:04:23 +0400

    Добавьте файлы проекта.

---
+++ Pr_Task1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Pr_Task1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine((i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "") + (i % 3 != 0 && i % 5 != 0 ? i.ToString() : ""));
+            }
+            Console.ReadKey();
+        }
+    }
+}
